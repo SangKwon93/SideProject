c0fa60b4cd86d4c44806f26ee79fe1bb7fb37f13
SangKwon93 <apfhda7@gmail.com>
2023-01-07 23:56:33 +0900

    fix & refactor : 2022년 월별로 멜론 OST TOp100 데이터 수집

--- melon_ost/ost_top100.py
+++ melon_ost/ost_top100.py
@@ -1,4 +1,3 @@
-# 수정본 부탁본
 import re
 import time
 import pymongo
@@ -30,34 +29,17 @@ from pymongo import MongoClient
 import random
 import requests
 
+# 브라우저 열기
 def get_chrome_driver():
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     driver = webdriver.Chrome('chromedriver.exe', options=options)
     return driver
 
-# def get_chrome_driver():
-#     chrome_options = webdriver.ChromeOptions()
-#     # chrome_options.headless = True
-
-#     driver = webdriver.Chrome(
-#         service=Service(ChromeDriverManager().install()),
-#         options=chrome_options
-#     )
-    
-#     return driver
 driver= get_chrome_driver()
 
-def ost_chart_date():
-    URL='https://www.melon.com/chart/month/index.htm?classCd=GN1500'
-    headers={"User-Agent": UserAgent().chrome}
-    res=requests.get(URL,headers=headers)
-    soup = BeautifulSoup(res.content,'html.parser')
-    date=soup.select_one('#conts > div.calendar_prid > span').text.strip()
-    return date
-
 def get_melon_chart():
-    melonList={}
+    musics={}
     URL='https://www.melon.com/chart/month/index.htm?classCd=GN1500'
     
     headers={
@@ -67,98 +49,78 @@ def get_melon_chart():
     driver.implicitly_wait(10)
     res=requests.get(URL,headers=headers)
     soup = BeautifulSoup(res.content,'html.parser')
-    top100=soup.select('.lst50,.lst100')
-    # # 화살표 클릭
-    # driver.find_element(By.XPATH,'//*[@id="conts"]/div[3]/div/button').click() 
-    # # 1월 클릭
-    # driver.find_element(By.XPATH,'//*[@id="conts"]/div[3]/div/div/dl/dd[1]/ul/li[1]/a').click()
-    # # 빈공간 클릭
-    # driver.find_element(By.XPATH,'//*[@id="conts"]/div[3]').click()
-    # month=driver.find_element(By.XPATH,f'#conts > div.calendar_prid > div > div > dl > dd.month_calendar > ul > li:nth-child({number}) > a > span')
     
-    for num in range(1,5):
-        data={}
-        # URL='https://www.melon.com/chart/month/index.htm?classCd=GN1500'
-        # driver.get(URL)
-        # driver.implicitly_wait(10) 
-        # 화살표 클릭
-        driver.find_element(By.XPATH,'//*[@id="conts"]/div[3]/div/button').click()
-        
-        # 1~12월 클릭
-        num_xpath=f'//*[@id="conts"]/div[3]/div/div/dl/dd[1]/ul/li[{num}]/a'
-        driver.find_element(By.XPATH,num_xpath).click()
-        # 월 클릭
-        
+    top100=soup.select('.lst50,.lst100')
+
+    # 월별로 도는 로직(12달)
+    for num in range(1,13):
+        this_month_musics = {}
+        URL='https://www.melon.com/chart/month/index.htm?classCd=GN1500'
+        driver.get(URL)
+        driver.implicitly_wait(10)
+        driver.find_element(By.XPATH,'//*[@id="conts"]/div[3]/div/button').click() # 화살표 클릭
+        num_xpath=f'//*[@id="conts"]/div[3]/div/div/dl/dd[1]/ul/li[{num}]/a' # 1~12월 클릭
+        driver.find_element(By.XPATH,num_xpath).click() # 월 클릭
         month_num=f'//*[@id="conts"]/div[3]/div/div/dl/dd[1]/ul/li[{num}]/a'
         month = driver.find_element(By.XPATH,month_num).get_attribute('data-date')
-        #data['ost_date']=month
-        # 빈공간
-        driver.find_element(By.XPATH,'//*[@id="conts"]/div[3]').click() 
+        driver.find_element(By.XPATH,'//*[@id="conts"]/div[3]').click() # 빈공간
+        day = driver.find_element(By.XPATH,'//*[@id="conts"]/div[3]/span').text
         
-        
-        #month=soup.select_one('#conts > div.calendar_prid > span').text.strip()
-        # month=soup.select_one('#conts > div.calendar_prid > span').text.strip()
-        # data['ost_date']=month
-        # res=requests.get(URL,headers=headers)
-        # soup = BeautifulSoup(res.content,'html.parser')
-        # top100=soup.select('.lst50,.lst100')
-        # month=soup.select_one('#conts > div.calendar_prid > span').text.strip()
-        # data={}
-        # data['chart_date']=soup.select_one('#conts > div.calendar_prid > span').text.strip()
-        # month=soup.select_one('#conts > div.calendar_prid > span').text.strip()
-        # data['ost_date']=month
-        stop=False
-        while True:
-            for i in top100:
-                data={}
-                
-                # month=soup.select_one('#conts > div.calendar_prid > span').text.strip()
-                data['ost_date']=month
-                # print(data)
-                # data['chart_date']=month
-                idx = str(i.select_one('.wrap.t_right > input')["value"])
-                num = i.select_one('span.rank_wrap').text.strip().split('\n')
-                stage = i.select_one('span.rank_wrap').text.strip().split('\n')
-                if len(num) == 1:
-                    num.append('0')
-                if '순위 동일' in stage:
-                    stage=''
-                elif '단계 상승' in stage:
-                    stage='+'
-                elif '단계 하락' in stage:
-                    stage="-"
-                elif '순위 진입' in stage:
-                    stage=''
-                
-                
-                
-                # 노래 정보
-                data['rank'] = i.select_one('.rank').text
-                data['up_down'] = f'{stage}{num[1]}'
-                data["title"] = i.select_one('.ellipsis.rank01').a.text
-                data["singer"] = i.select_one('.ellipsis.rank02').a.text
-                data['album'] = i.select_one('.ellipsis.rank03').a.text
-                data['album_code']=i.select_one('.wrap_song_info > div.ellipsis.rank03> a')["href"].split("'")[1]
-                
-                # 노래 고유번호 인덱스 활용
-                data["index"] = idx
-                
-            # data={}
-            # for num in range(1,13):
-            #     data={}
-            #     # URL='https://www.melon.com/chart/month/index.htm?classCd=GN1500'
-            #     # driver.get(URL)
-            #     # driver.implicitly_wait(10) 
-            #     # 화살표 클릭
-            #     driver.find_element(By.XPATH,'//*[@id="conts"]/div[3]/div/button').click()
-                
-            #     # 1~12월 클릭
-            #     num_xpath=f'//*[@id="conts"]/div[3]/div/div/dl/dd[1]/ul/li[{num}]/a'
-            #     driver.find_element(By.XPATH,num_xpath).click()
+        # URL2 는 Network - Fetch/XHR에서 월별로 URL2 주소가 변경됨을 확인함
+        # (기존 URL은 변동이 없고 URL2가 변경) 
+        URL2=f'https://www.melon.com/chart/month/index.htm?classCd=GN1500&moved=Y&rankMonth={month}'
+        headers={"User-Agent": UserAgent().chrome}
+        driver.get(URL2)
+        driver.implicitly_wait(10)
+        driver.maximize_window()
+        res=requests.get(URL2,headers=headers)
+        soup = BeautifulSoup(res.content,'html.parser')
+        top100=soup.select('.lst50,.lst100')
 
-                # 속도 측면에서 index를 활용! 
-                melonList[idx] = data
-            return melonList
-        # print(melonList)
-            # chart_date=ost_chart_date()
-print(get_melon_chart())
+        # 곡별로 도는 로직(100곡)
+        for i in top100:
+            music = {}
+            idx = str(i.select_one('.wrap.t_right > input')["value"])
+            num = i.select_one('span.rank_wrap').text.strip().split('\n')
+            stage = i.select_one('span.rank_wrap').text.strip().split('\n')
+            if len(num) == 1:
+                num.append('0')
+            if '순위 동일' in stage:
+                stage=''
+            elif '단계 상승' in stage:
+                stage='+'
+            elif '단계 하락' in stage:
+                stage="-"
+            elif '순위 진입' in stage:
+                stage=''
+            music['rank'] = i.select_one('.rank').text
+            music['day'] = day
+            music['up_down'] = f'{stage}{num[1]}'
+            music["title"] = i.select_one('.ellipsis.rank01').a.text
+            music["singer"] = i.select_one('.ellipsis.rank02').a.text
+            music['album'] = i.select_one('.ellipsis.rank03').a.text
+            music['album_code']=i.select_one('.wrap_song_info > div.ellipsis.rank03> a')["href"].split("'")[1]
+            music['ost_date']=month
+            music["index"] = idx
+            this_month_musics[idx] = music
+        
+        # 달별로 한번 더 묶기
+        # 한번 더 딕셔너리 형태로 묶은 이유는 인덱스로 묶게되면 12달간 겹치는 곡들을 하나의 값으로 잘못 인식
+        musics[month] = this_month_musics
+    return musics
+
+output = get_melon_chart()
+
+# 시각화를 위해 csv파일 저장
+df= pd.DataFrame(output)
+df.to_csv("ost_melon_top100_2022.csv")
+
+'''
+해결하기 어려웠던 점
+Q : 자꾸 for문 안에서 돌아가며 데이터를 받아왔는데 딕셔너리에 하나의 값만 들어온다.
+A : 빈 딕셔너리 변수를 for문 안에서 돌리면 계속 초기화되며 돌아간다. 
+
+Q : 하나의 값만 반환된다.
+A : return이 진행되면 종료된다. for문 돌릴 때 들여쓰기 주의할것! 
+
+'''
